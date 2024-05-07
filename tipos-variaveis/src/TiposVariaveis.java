8504018f087c1db53c9a4263eb9fa75d59e166c7
Irving Caetano <irving_vrsouza@hotmail.com>
2024-05-07 17:16:34 -0300

    aula 2 tipos variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 1272345;
+        long cpf = 12345678900L;
+        float pi = 3.14F;
+        double salario = 1413.32;
     }
+        
 }
